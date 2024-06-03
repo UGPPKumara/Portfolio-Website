4894b487d670ab05e55765f9743970c0c133990f
Prasanna Pushpa Kumara <prasannapushpakumaracampus@gmail.com>
2024-06-03 17:55:04 +0530

    add files

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -1,8 +1,23 @@
+// eslint-disable-next-line no-unused-vars
 import React from 'react'
+import  './Portfolio.css'
+import image1 from './../../assets/img/SMS.png'
+
 
 const Portfolio = () => {
   return (
-    <div>Portfolio</div>
+    <div className="my-portfolio">
+      <h1 className='title'>My <span>Portfolio</span></h1>
+      <div className="portfolio-cards">
+        <div className="card">
+            <img src={image1} alt=""/>
+          <h1 className="card-title">
+            App UI design
+          </h1>
+          
+        </div>
+      </div>
+    </div>
   )
 }
 
